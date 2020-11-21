2bb3992218679ffb259caf3d42e805d5b91456d1
maniek <mpastuszka@gmail.com>
2020-11-21 13:31:50 +0100

    [init] - project initialization

---
+++ src/test/java/pl/sda/javapol4/springdemojavapol4/SpringDemoJavapol4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.javapol4.springdemojavapol4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoJavapol4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
