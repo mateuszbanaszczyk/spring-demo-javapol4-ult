d93f01877a3791102a0b0e200e3a40febcaa1aeb
Mateusz Banaszczyk <mati.banaszczyk@gmail.com>
2020-11-22 19:12:02 +0100

    [init] - project initialization

---
+++ src/main/java/pl/sda/javapol4/spiringdemojavapol4ult/SpiringDemoJavapol4UltApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.javapol4.spiringdemojavapol4ult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiringDemoJavapol4UltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiringDemoJavapol4UltApplication.class, args);
+    }
+
+}
