4f11b32cf7e3ee7f84d42ccbef350c7e6623115b
Mateusz Banaszczyk <mati.banaszczyk@gmail.com>
2020-11-22 19:12:02 +0100

    [merge] - merge origin master with develop

---
+++ src/test/java/pl/sda/javapol4/spiringdemojavapol4ult/SpiringDemoJavapol4UltApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.javapol4.spiringdemojavapol4ult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiringDemoJavapol4UltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
