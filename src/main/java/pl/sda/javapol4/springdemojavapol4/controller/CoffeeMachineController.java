9cc480d2793c42cd39926d6af18dee79f1aa4ed0
Mateusz Banaszczyk <mati.banaszczyk@gmail.com>
2020-11-29 13:54:40 +0100

    [change] - ability to obtain request parameters

--- src/main/java/pl/sda/javapol4/springdemojavapol4/controller/CoffeeMachineController.java
+++ src/main/java/pl/sda/javapol4/springdemojavapol4/controller/CoffeeMachineController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.sda.javapol4.springdemojavapol4.service.CoffeeMachineService;
 
 @Slf4j
@@ -12,6 +13,8 @@ import pl.sda.javapol4.springdemojavapol4.service.CoffeeMachineService;
 @RequestMapping("/coffee")
 public class CoffeeMachineController {
 
+    public static final String NAME = "name";
+    public static final String SURNAME = "name";
     private static final String COFFEE_MACHINES = "coffeeMachines";
     private final CoffeeMachineService coffeeMachineService;
 
@@ -20,12 +23,19 @@ public class CoffeeMachineController {
         this.coffeeMachineService = coffeeMachineService;
     }
 
+//    user name and surname
+//    ?imie=mateusz&nazwisko=banaszczyk
     @GetMapping("/all-coffee-machines")
-    public String myCoffeeMachines(Model model){
+    public String myCoffeeMachines(Model model, @RequestParam(value ="imie", required = false, defaultValue = "Janek") String name,
+                                                @RequestParam(value ="nazwisko", required = false, defaultValue = "Kowalski") String surname){
         log.info("my all coffee machines request");
+        log.info("name = [{}], surmane = [{}], name, surname");
+//        log.info("name = [${name}], surmane = [${surname}]"); == string interpolation kotlin
 
         var allCoffeeMachines = coffeeMachineService.giveMeMyCoffeeMachines();
        model.addAttribute(COFFEE_MACHINES, allCoffeeMachines);
+       model.addAttribute(NAME, name);
+       model.addAttribute(SURNAME, surname);
         return "coffees/my-coffees";
     }
 }
