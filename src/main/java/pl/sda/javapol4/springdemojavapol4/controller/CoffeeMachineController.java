94eb1e54385b14a104b0f066152a2299c7399ed4
Mateusz Banaszczyk <mati.banaszczyk@gmail.com>
2020-11-29 13:08:28 +0100

    [add] - table with coffee machines

--- src/main/java/pl/sda/javapol4/springdemojavapol4/controller/CoffeeMachineController.java
+++ src/main/java/pl/sda/javapol4/springdemojavapol4/controller/CoffeeMachineController.java
@@ -2,6 +2,7 @@ package pl.sda.javapol4.springdemojavapol4.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.javapol4.springdemojavapol4.service.CoffeeMachineService;
@@ -11,6 +12,7 @@ import pl.sda.javapol4.springdemojavapol4.service.CoffeeMachineService;
 @RequestMapping("/coffee")
 public class CoffeeMachineController {
 
+    private static final String COFFEE_MACHINES = "coffeeMachines";
     private final CoffeeMachineService coffeeMachineService;
 
 
@@ -19,10 +21,11 @@ public class CoffeeMachineController {
     }
 
     @GetMapping("/all-coffee-machines")
-    public String myCoffeeMachines(){
+    public String myCoffeeMachines(Model model){
         log.info("my all coffee machines request");
 
         var allCoffeeMachines = coffeeMachineService.giveMeMyCoffeeMachines();
+       model.addAttribute(COFFEE_MACHINES, allCoffeeMachines);
         return "coffees/my-coffees";
     }
 }
