f0b342449b0b60724baf0874eb3199c1c8e3a5e9
Mateusz Banaszczyk <mati.banaszczyk@gmail.com>
2020-12-05 11:28:08 +0100

    [add] - application/xml

--- src/main/java/pl/sda/javapol4/springdemojavapol4/controller/RestCoffeMachineController.java
+++ src/main/java/pl/sda/javapol4/springdemojavapol4/controller/RestCoffeMachineController.java
@@ -2,12 +2,14 @@ package pl.sda.javapol4.springdemojavapol4.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.javapol4.springdemojavapol4.entity.CoffeeMachine;
 import pl.sda.javapol4.springdemojavapol4.service.CoffeeMachineService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/rest")
@@ -20,8 +22,11 @@ public class RestCoffeMachineController {
     public RestCoffeMachineController(CoffeeMachineService coffeeMachineService) {
         this.coffeeMachineService = coffeeMachineService;
     }
-    @GetMapping("/all-coffee-machines")
+//    @GetMapping("/all-coffee-machines")
+    @GetMapping(value = "/all-coffee-machines",
+    produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<CoffeeMachine> myCoffeeMachines(){
+        log.info("rest controller - all coffee machines");
         return coffeeMachineService.giveMeMyCoffeeMachines();
     }
 }
