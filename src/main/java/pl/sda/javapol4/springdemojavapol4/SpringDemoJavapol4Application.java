bca63ab80654676475063410251fa18a189cbd61
Mateusz Banaszczyk <mati.banaszczyk@gmail.com>
2021-01-14 22:00:16 +0100

    [add] - db support for coffee machines2

--- src/main/java/pl/sda/javapol4/springdemojavapol4/SpringDemoJavapol4Application.java
+++ src/main/java/pl/sda/javapol4/springdemojavapol4/SpringDemoJavapol4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDemoJavapol4Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringDemoJavapol4Application.class, args);
     }
 
